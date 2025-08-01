30eaacd739d073d2769fe9253bcff2b241a25aaf
Christensen Cadeau <christensencadeau@icloud.com>
2025-08-01 16:52:20 -0400

    Update `ToDo` component content and enhance `HomePage` styling
    - Add image and detailed content with styling to `ToDo` component.
    - Adjust `HomePage` image alignment with responsive styling.

--- doowit-app/src/components/HomePage.jsx
+++ doowit-app/src/components/HomePage.jsx
@@ -4,7 +4,7 @@ import ToDo from "./ToDo";
 export default function HomePage() {
     return (
         <main className={"d-flex flex-column"}>
-            <img src={dance} className={"w-100 ms-2"} alt="Illustration of three people dancing"/>
+            <img src={dance} className={"img-fluid ms-3"} alt="Illustration of three people dancing"/>
             <h1 className={"hachi-maru-pop-regular mb-3"}>To Do+</h1>
             <ToDo />
         </main>
--- doowit-app/src/components/ToDo.jsx
+++ doowit-app/src/components/ToDo.jsx
@@ -1,5 +1,13 @@
-export default function ToDo(){
+import moon from "../assets/moon.png"
+
+export default function ToDo() {
     return (
-        <div className={"bg-danger"}>working</div>
+        <div>
+            <img src={moon} className={"img-fluid"} alt={"moon icon"}/>
+            <h2>Shop Groceries</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec diam metus, tristique nec erat id,
+                vehicula posuere orci. Praesent sit amet eleifend tortor. Aliquam dignissim convallis ornare. Etiam eget
+                elit in risus laoreet sodales. Sed a iaculis mauris. Praesent imperdiet.</p>
+        </div>
     )
 }
