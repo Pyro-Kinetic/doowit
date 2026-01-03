b112a8ff52c048dbb747ec5a7ae08bb40d0b8c3d
Christensen Cadeau <christensencadeau@icloud.com>
2026-01-02 19:32:22 -0500

    Redesign `LoginPage` layout and add illustration
    - Update `LoginPage` to use a two-pane layout for improved aesthetics.
    - Add a dancing illustration to the left pane for visual enhancement.
    - Refactor styles and structure for better responsiveness and alignment.

--- doowit-app/src/components/LoginPage.jsx
+++ doowit-app/src/components/LoginPage.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import Register from "./Register";
 import Login from "./Login";
+import danceGraphic from "../assets/dancing.jpg"
 
 export default function LoginPage() {
     const [showRegisterModal, setShowRegisterModal] = useState(false);
@@ -20,15 +21,23 @@ export default function LoginPage() {
     }
 
     return (
-        <div className="login-page container d-flex flex-column align-items-center justify-content-center" style={{minHeight: "80vh"}}>
-            <h2 className="hachi-maru-pop-regular rich-black mb-4">Welcome to Doowit!</h2>
-            <div className="d-flex gap-3">
-                <button onClick={toggleLoginModal} className="btn planet-background planet-hover text-light roboto">Login</button>
-                <button onClick={toggleRegisterModal} className="btn sun-background sun-hover text-dark roboto">Sign Up</button>
+        <div className="login-page home-layout container" style={{minHeight: "80vh"}}>
+            <div className="left-pane d-flex align-items-center justify-content-center">
+                <img src={danceGraphic} className="app-image img-fluid" alt="Illustration of three people dancing"/>
             </div>
 
-            {showRegisterModal && <Register handleBackdropClick={handleBackdropClick} />}
-            {showLoginModal && <Login handleBackdropClick={handleBackdropClick} />}
+            <div className="right-pane d-flex flex-column align-items-center justify-content-center">
+                <h2 className="hachi-maru-pop-regular rich-black mb-4">Welcome to Doowit!</h2>
+                <div className="d-flex gap-3">
+                    <button onClick={toggleLoginModal} className="btn planet-background planet-hover text-light roboto">Login
+                    </button>
+                    <button onClick={toggleRegisterModal} className="btn sun-background sun-hover text-dark roboto">Sign Up
+                    </button>
+                </div>
+            </div>
+
+            {showRegisterModal && <Register handleBackdropClick={handleBackdropClick}/>}
+            {showLoginModal && <Login handleBackdropClick={handleBackdropClick}/>}
         </div>
     );
 }
